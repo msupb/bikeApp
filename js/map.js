1cb19c054acc5120b3549862e5e31c7ac2dbd8e3
gokberknur <gokberknur@gmail.com>
2017-11-30 10:58:49 +0100

    getting coordinates added

--- js/map.js
+++ js/map.js
@@ -43,3 +43,21 @@ function initMap() {
           }
         });
       }
+
+if (navigator.geolocation) {
+    console.log('Geolocation is supported!');
+}
+else {
+    console.log('Geolocation is not supported for this Browser/OS.');
+}
+
+window.onload = function() {
+    var startPos;
+    var geoSuccess = function(position) {
+        startPos = position;
+        document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+        document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+};
